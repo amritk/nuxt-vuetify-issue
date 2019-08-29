c1b41bf58925c6c5f23a902800d5073b19033deb
Amrit Kahlon <amrit@hockey-community.com>
2019-08-29 16:03:22 -0700

    feat: Initial commit 🎉

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app" style="width:100%;">
+    <v-app id="inspire">
+      <v-tabs centered>
+        <v-tab>Tab 1</v-tab>
+        <v-tab>Tab 2</v-tab>
+        <v-tab>Tab 3</v-tab>
+        <v-tab>Tab 4</v-tab>
+        <logo></logo>
+        <icon-link></icon-link>
+      </v-tabs>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import "vuetify/dist/vuetify.css";
+
+export default {
+};
+</script>
+
+<style scoped>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #526488;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ vuetify.options.js
@@ -0,0 +1,23 @@
+import recursiveReaddir from 'recursive-readdir'
+
+console.log('vuetify.options.js is being hit', process.client)
+
+const localComponents = {}
+recursiveReaddir('components', (err, items) => {
+  console.log('error: ', err);
+  console.log('components: ', items);
+  for (const item of items) {
+    localComponents[item.replace(/^(.*[\\/])/, '').slice(0, -4)] = item.replace('src', '')
+  }
+})
+
+export default {
+  loaderOptions: {
+    match: (_originalTag, { kebabTag, camelTag }) => {
+      const comp = localComponents[kebabTag]
+      if (comp) {
+        return [camelTag, `import ${camelTag} from '@${comp}'`]
+      }
+    }
+  }
+}
